c3ce551dcbe810e89f8b91222ee500205e0ba7d6
Amartya Majumdar <a.majumdar1041@gmail.com>
2025-05-13 17:57:18 +0530

    ...

--- random/main.tf
+++ random/main.tf
@@ -1,6 +1,6 @@
 resource "random_pet" "increase_resource_count" {
   for_each = { for idx in range(1, 10) : idx => idx }
 
-  prefix = "${pet_prefix}-${each.key}"
+  prefix = "${var.pet_prefix}-${each.key}"
   length = 2
 }
