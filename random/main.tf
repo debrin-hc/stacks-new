95640835ca1d4bf0d758dbf1c32241ca1582bbf6
Amartya Majumdar <a.majumdar1041@gmail.com>
2025-05-21 12:08:04 +0530

    adding null resource

--- random/main.tf
+++ random/main.tf
@@ -6,11 +6,7 @@ resource "random_pet" "increase_resource_count" {
 }
 
 resource "null_resource" "example" {
-  # Using triggers to force execution on every apply
   triggers = {
     always_run = timestamp()
-  }
-  provisioner "local-exec" {
-    command = "echo This specific command will execute every time during apply as triggers are used"
   }
 }
