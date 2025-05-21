10883e4f6d8aced1a5c66bb2ca9e05301fe6d569
Amartya Majumdar <a.majumdar1041@gmail.com>
2025-05-21 12:05:49 +0530

    adding null resource

--- providers.tfstack.hcl
+++ providers.tfstack.hcl
@@ -3,6 +3,13 @@ required_providers {
     source  = "hashicorp/random"
     version = "~> 3.7.2"
   }
+
+  null = {
+    source = "hashicorp/null"
+    version = "3.2.4"
+  }
 }
 
 provider "random" "this" {}
+
+provider "null" "this" {}
--- random/main.tf
+++ random/main.tf
@@ -4,3 +4,13 @@ resource "random_pet" "increase_resource_count" {
   prefix = "pet-${each.key}"
   length = 2
 }
+
+resource "null_resource" "example" {
+  # Using triggers to force execution on every apply
+  triggers = {
+    always_run = timestamp()
+  }
+  provisioner "local-exec" {
+    command = "echo This specific command will execute every time during apply as triggers are used"
+  }
+}
