4f577fae556d0d02fca70edaec2acb7dd51efdff
Amartya Majumdar <a.majumdar1041@gmail.com>
2025-05-21 11:57:02 +0530

    increasing resources

--- random/main.tf
+++ random/main.tf
@@ -1,5 +1,5 @@
 resource "random_pet" "increase_resource_count" {
-  for_each = { for idx in range(1, 10) : idx => idx }
+  for_each = { for idx in range(1, 20) : idx => idx }
 
   prefix = "pet-${each.key}"
   length = 2
